35d9ea6febd1dcace37a87fcf964f6a8204a3b80
Yenneferr <Yenneferr@users.noreply.github.com>
2019-10-15 20:35:45 -0600

    using arguments as issue#3

--- genetic_algorithm/Main.cpp
+++ genetic_algorithm/Main.cpp
@@ -5,43 +5,48 @@
 #include "Chromossome.h"
 #include "GeneticAlgorithm.h"
 
-#define MAX_ITER 2
-#define POP_SIZE 1000
-#define MUT_RATE 0.07
-#define ELIT_RATE 0.15
-#define MAX_GEN 200
+
 
 int main()
 {
-	int n;
-	std::cin >> n;
-	Data::adjacencyMatrix =
-		std::vector <std::vector <int> >(n, std::vector <int>(n));
+	#defining constants of the program:
+	int MAX_ITER = 2;
+	int POP_SIZE = 1000;
+	float MUT_RATE = 0.07;
+	float ELIT_RATE = 0.15;
+	int MAX_GEN = 200;
+	execute(MAX_ITER, POP_SIZE, MUT_RATE, ELIT_RATE, MAX_GEN);
+}
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			std::cin >> Data::adjacencyMatrix[i][j];
+int execute(int MAX_ITER,int  POP_SIZE, float MUT_RATE,float ELIT_RATE, int MAX_GEN)
+{
+    int n;
+    std::cin >> n;
+    Data::adjacencyMatrix =
+            std::vector <std::vector <int> >(n, std::vector <int>(n));
 
-	for (int i = 0; i < MAX_ITER; i++)
-	{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            std::cin >> Data::adjacencyMatrix[i][j];
+
+    for (int i = 0; i < MAX_ITER; i++)
+    {
         std::cout << "Iteration " << i+1 << ":\n";
-		GeneticAlgorithm ga(POP_SIZE, n, MUT_RATE, ELIT_RATE);
-		ga.evaluate();
-		double best = ga.getBestChromossome().getFitness();
-		for (int t = 0; t < MAX_GEN; t++) {
-			ga.newGeneration();
-			ga.evaluate();
-			double curBest = ga.getBestChromossome().getFitness();
-			if (curBest < best)
-			{
-				t = 0;
-				best = curBest;
-			}
-		}
-		ga.report();
+        GeneticAlgorithm ga(POP_SIZE, n, MUT_RATE, ELIT_RATE);
+        ga.evaluate();
+        double best = ga.getBestChromossome().getFitness();
+        for (int t = 0; t < MAX_GEN; t++) {
+            ga.newGeneration();
+            ga.evaluate();
+            double curBest = ga.getBestChromossome().getFitness();
+            if (curBest < best)
+            {
+                t = 0;
+                best = curBest;
+            }
+        }
+        ga.report();
         std::cout << std::endl;
-	}
-
-
-	return 0;
+    }
+    return 0;
 }
